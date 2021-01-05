ed711627eb0ec6902632825a9c0da1703c99fc45
nishimura <nishimura@r.recruit.co.jp>
2021-01-05 18:32:06 +0900

    edit header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
+  { label: 'Memo', page: '/memo' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
